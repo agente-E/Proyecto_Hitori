297e9565b894542bedc64d7aee569da61f8765f3
agente-E <alexpalmada89@gmail.com>
2024-06-19 12:13:20 +0200

    First commit

---
+++ Scripts/Start_Menu/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Classroom/classroom.tscn")
+
+
+func _on_credits_pressed():
+	pass # Replace with function body.
